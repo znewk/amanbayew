24e72764d87fd2896b295f6602118a13ee870136
znewk <zane.css34@gmail.com>
2023-11-07 09:07:30 +0600

    intro block ligther theme

--- src/components/IntroBlock/index.js
+++ src/components/IntroBlock/index.js
@@ -1,15 +1,26 @@
 import styles from './style.module.css'
 import {Image} from "react-bootstrap";
 import {useLocalStorage} from "primereact/hooks";
+import classnames from "classnames"
 
 const IntroBlock = () => {
     const [theme, setTheme] = useLocalStorage('dark', 'theme');
+
+    const isDark = () => {
+        if(theme == 'dark'){
+            return true
+        } else {
+            return false
+        }
+    }
+
+
     return (
         <div className={styles.container}>
             <div className={styles.gifBody}>
                 <Image src={'/cat.gif'} className={styles.gif}/>
             </div>
-            <div className={styles.helloMessage}>
+            <div className={classnames(styles.helloMessage, !isDark() && styles.lightMessage )}>
                 привет, я инди-разработчик из астаны!
             </div>
 
